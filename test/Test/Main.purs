79c959c090c8de85ba22a07686deef9e99287650
joneshf <jones3.hardy@gmail.com>
2019-05-11 11:43:23 -0700

    Use qualified imports
    Per review, using unqualified imports tends to lead to merge conflicts.
    We can mitigate that by qualifying imports.

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -8,66 +8,66 @@ import Effect (Effect)
 import Effect.Console (errorShow, infoShow, logShow)
 import Node.Buffer (toString)
 import Node.Encoding (Encoding(..))
-import Node.Net (address, close, createConnectionTCP, createServer, endString, listenTCP, localAddress, localPort, onCloseServer, onCloseSocket, onConnect, onConnection, onData, onDrain, onEnd, onErrorServer, onErrorSocket, onListening, onLookup, onReady, onTimeout, writeString)
+import Node.Net as Node.Net
 
 main :: Effect Unit
 main = do
-  server <- createServer mempty \socket -> do
+  server <- Node.Net.createServer mempty \socket -> do
     infoShow { _message: "Server received connection" }
-    void $ writeString socket "Hello socket\n" UTF8 mempty
-    endString socket "Server is ending connection\n" UTF8 mempty
+    void $ Node.Net.writeString socket "Hello socket\n" UTF8 mempty
+    Node.Net.endString socket "Server is ending connection\n" UTF8 mempty
 
-  onCloseServer server do
+  Node.Net.onCloseServer server do
     infoShow { _message: "Server closed" }
-  onConnection server \socket -> do
-    addr <- localAddress socket
-    port <- localPort socket
+  Node.Net.onConnection server \socket -> do
+    addr <- Node.Net.localAddress socket
+    port <- Node.Net.localPort socket
     infoShow { _message: "Connection successfully made", addr, port }
-  onErrorServer server \err -> do
+  Node.Net.onErrorServer server \err -> do
     infoShow { _message: "Server had an error", err }
-  onListening server do
+  Node.Net.onListening server do
     infoShow { _message: "Server is listening" }
 
-  listenTCP server 0 "localhost" 511 do
-    addr <- address server
+  Node.Net.listenTCP server 0 "localhost" 511 do
+    addr <- Node.Net.address server
     infoShow { _message: "Opened server", addr }
     case addr of
       Just (Left { port }) -> do
-        socket <- createConnectionTCP port "localhost" do
+        socket <- Node.Net.createConnectionTCP port "localhost" do
           infoShow { _message: "Connected to server" }
 
-        onCloseSocket socket case _ of
+        Node.Net.onCloseSocket socket case _ of
           false -> infoShow { _message: "Socket closed without an error" }
           true -> errorShow { _message: "Socket closed with an error" }
-        onConnect socket do
+        Node.Net.onConnect socket do
           infoShow {_message: "Socket connected"}
-        onData socket case _ of
+        Node.Net.onData socket case _ of
           Left buffer -> do
             logShow { _message: "Received some data", buffer }
             bufferString <- toString UTF8 buffer
             logShow { _message: "Converted to a `String`", buffer, bufferString }
           Right string -> logShow { _message: "Received some data", string }
-        void $ onDrain socket do
+        void $ Node.Net.onDrain socket do
           infoShow { _message: "Socket drained" }
-        onEnd socket do
+        Node.Net.onEnd socket do
           infoShow { _message: "Socket ended, closing server" }
-          close server mempty
-        onErrorSocket socket \err ->
+          Node.Net.close server mempty
+        Node.Net.onErrorSocket socket \err ->
           errorShow { _message: "Socket had an error", err }
-        onLookup socket case _ of
+        Node.Net.onLookup socket case _ of
           Left err ->
             infoShow { _message: "Socket had an error resolving DNS", err }
           Right lookup ->
             infoShow { _message: "Socket successfully resolved DNS", lookup }
-        onReady socket do
+        Node.Net.onReady socket do
           infoShow { _message: "Socket is ready" }
-          void $ writeString socket "Hello server" UTF8 mempty
-        onTimeout socket do
+          void $ Node.Net.writeString socket "Hello server" UTF8 mempty
+        Node.Net.onTimeout socket do
           infoShow { _message: "Socket timed out" }
-          endString socket "Closing connection" UTF8 mempty
+          Node.Net.endString socket "Closing connection" UTF8 mempty
 
       Just (Right endpoint) -> do
         errorShow { _message: "Server unexpectedly connected over ICP" }
-        close server mempty
+        Node.Net.close server mempty
 
-      _ -> close server mempty
+      _ -> Node.Net.close server mempty
